f23e69a21d03d3c94a530aaaada67a1185541ccc
LAPTOP-L16M6SQ8\admin <amonrat.c@cubesofttech.com>
2025-04-23 13:34:25 +0700

    new api

---
+++ config/database.js
@@ -0,0 +1,9 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('my_api_db', 'my_user', 'secure_password', {
+    host: 'localhost',
+    dialect: 'postgres',
+   
+});
+
+module.exports = sequelize;
---
+++ models/User.js
@@ -0,0 +1,39 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const User = sequelize.define('User', {
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    createdAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW
+    },
+    updatedAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW
+    },
+    createdat: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW
+    },
+    updatedat: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW
+    }
+},{
+    timestamps: true, // Enable timestamps
+});
+
+module.exports = User;
