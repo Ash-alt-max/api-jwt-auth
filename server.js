369d734f13ad301415603b868ec0a12b4823b067
LAPTOP-L16M6SQ8\admin <amonrat.c@cubesofttech.com>
2025-04-30 10:06:43 +0700

    commit 1

--- server.js
+++ server.js
@@ -30,3 +30,6 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
+
+console.log('Server running at http://localhost:5000');
+
